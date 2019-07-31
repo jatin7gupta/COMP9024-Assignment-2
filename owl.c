63f3a4d6703236d92f557202a8eac29f588f3638
Jatin Gupta <guptajatin@outlook.com>
2019-08-01 08:47:51 +1000

    Phase 1 complete

---
+++ owl.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+int len(char *word) {
+	return strlen(word);
+}
+
+bool differByOne(char *firstWord, char *secondWord) {
+	int firstWordLength = len(firstWord);
+	int secondWordLength = len(secondWord);
+	int changeCount = 0;
+	if (firstWordLength == secondWordLength) {
+		for (int f = 0, s = 0; f < firstWordLength && s < secondWordLength; f++, s++) {
+			if (firstWord[f] != secondWord[f]) {
+				changeCount++;
+			}
+		}
+		if (changeCount == 1) {
+			return true;
+		}
+	} else if (abs(firstWordLength - secondWordLength) == 1) {
+		int f = 0;
+		int s = 0;
+		while (f < firstWordLength && s < secondWordLength) {
+			if (firstWord[f] != secondWord[s]) {
+				if (firstWordLength > secondWordLength) {
+					f++;
+				} else {
+					s++;
+				}
+				changeCount++;
+			} else {
+				f++;
+				s++;
+			}
+		}
+		if (changeCount == 1) {
+			return true;
+		}
+	}
+	return false;
+	
+}
+
+int main(void) {
+	printf("%d\n", differByOne("cann","can"));
+	return EXIT_SUCCESS;
+}
