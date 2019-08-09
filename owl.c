370e4a98bf797a5ba585bd54f93708c8741b379d
Jatin Gupta <guptajatin@outlook.com>
2019-08-09 15:28:59 +1000

    updated comments

--- owl.c
+++ owl.c
@@ -1,3 +1,12 @@
+/*
+Name: Jatin Gupta
+zID / email: z5240221@ad.unsw.edu.au
+Date last modified: 09/08/2019
+Name of course and session: COMP9024 19T2
+Task: OWL
+Link to spec: https://webcms3.cse.unsw.edu.au/COMP9024/19T2/resources/28573 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -10,12 +19,21 @@
 #define WORD_LENGTH 21
 #define MAXIMUM_PATHS 100
 
-
+/*
+Input: array of characters
+Return Value: int
+Usage: Takes a array of word, returns the length of the array 
+*/
 int len(char *word) {
 	return strlen(word);
 }
 
 
+/*
+Input: character pointer array and integer count of words
+Return Value: 2d character pointer array which has been freed and now is NULL
+Usage: Free up the malloc-ed space
+*/
 char **freeDict(char **dict, int wordCount) {
 	for (int i = 0; i < wordCount; i++) {
 		free(dict[i]);
@@ -27,6 +45,11 @@ char **freeDict(char **dict, int wordCount) {
 }
 
 
+/*
+Input: integer, to give the malloc size
+Return Value: character pointer array
+Usage: To allocate memory to the dictionary
+*/
 char **performMalloc(int length) {
 	char **dict = malloc(length * sizeof(char *));
 	if (dict == NULL) {
@@ -37,6 +60,11 @@ char **performMalloc(int length) {
 }
 
 
+/*
+Input: integer, to give the malloc size
+Return Value: array of character of the given size
+Usage: to give memory to a word as array of characters
+*/
 char *mallocWord(int length) {
 	char *block = malloc(length * sizeof(char));
 	if (block == NULL) {
@@ -47,6 +75,11 @@ char *mallocWord(int length) {
 }
 
 
+/*
+Input: array of char pointers and integer, to give the realloc size
+Return Value: array of character pointers after giving them new memory of the given size
+Usage: to give memory to each word and increase by twice when they fill up
+*/
 char **performRealloc(char **dict, int length) {
 	dict = realloc(dict, length * 2 * sizeof(char *));
 	if (dict == NULL) {
@@ -57,6 +90,11 @@ char **performRealloc(char **dict, int length) {
 }
 
 
+/*
+Input: integer, to give the malloc size
+Return Value: array of integers after giving them memory of the given size and assigning each value to -1
+Usage: to make an array and assign them a constant value to all the elements of the array
+*/
 int *mallocArray(int length) {
 	int *block = malloc(length * sizeof(int));
 	if (block == NULL) {
@@ -70,6 +108,11 @@ int *mallocArray(int length) {
 }
 
 
+/*
+Input: two array of characters
+Return Value: true if they differ by one, else false. Differ by one is explained in the assignment specs
+Usage: to check if two strings (character arrays) are differing by one or not
+*/
 bool differByOne(char *firstWord, char *secondWord) {
 	int firstWordLength = len(firstWord);
 	int secondWordLength = len(secondWord);
@@ -106,7 +149,11 @@ bool differByOne(char *firstWord, char *secondWord) {
 	return false;
 }
 
-
+/*
+Input: pointer to the array of character pointers and integer, initial malloced size 
+Return Value: integer, the number of words which has been read by the function 
+Usage: to read input from stdin and store them into an array of character pointers and returning the caller the number of words read
+*/
 int takeInput(char ***dict, int initialLength) {
 	int wordCounter = 0;
 	char word[WORD_LENGTH];
@@ -130,6 +177,11 @@ int takeInput(char ***dict, int initialLength) {
 }
 
 
+/*
+Input: array of pointer to the character array and integer, count of the number of words
+Return Value: Graph which has the vertices and edges
+Usage: to create the graph for the read words
+*/
 Graph createGraph(char **dict, int wordCount){
     Graph g = newGraph(wordCount);
     for (int i = 0; i < wordCount; i++) {
@@ -142,6 +194,12 @@ Graph createGraph(char **dict, int wordCount){
     return g;
 }
 
+
+/*
+Input: array of pointer to the character array and integer, count of the number of words and the Graph 
+Return Value: void
+Usage: to print the value of the of all the words in the dict and call the showGraph from the Graph ADT
+*/
 void printGraph(char **dict, int wordCount, Graph graph) {
 	printf("Dictionary\n");
 	for (int i = 0; i < wordCount; i++) {
@@ -152,8 +210,11 @@ void printGraph(char **dict, int wordCount, Graph graph) {
 }
 
 
-
-
+/*
+Input: array of pointer to the character array and integer, count of the number of words and the Graph 
+Return Value: void
+Usage: to print the value of the of all the words in the dict and call the showGraph from the Graph ADT
+*/
 bool childExist(int v, Graph g, int numV) {
 	for (int w = v+1; w < numV; w++) {
 		if (isEdge(newEdge(v, w), g)) {
@@ -163,6 +224,14 @@ bool childExist(int v, Graph g, int numV) {
 	return false;
 }
 
+
+/*
+Input: Graph, vertix to start with (integer), number of vertixes (integer), a counter which counts the depth (integer), previous maximum seen value, length of path (pointer to the integer), 
+		visited array to store the temp path (array of int), cursor to backtrack (pointer to the integer), array of quacks to store the paths (array of quacks), 
+		number of paths (pointer to the integer)   
+Return Value: void
+Usage: to recurse till the node when we do not have any paths to go further and check of you have seen a bigger path or same length path, if yes save that path into a quack
+*/
 void dfsR(Graph g, Vertex v, int numV, int counter, int *maxSeen, int *visited, int *cursor, Quack *quackArray, int *path) {
 	visited[*cursor] = v;
     for (Vertex w = v+1; w < numV; w++) {
@@ -175,7 +244,7 @@ void dfsR(Graph g, Vertex v, int numV, int counter, int *maxSeen, int *visited, 
 					*maxSeen = counter+1;
 					
 					*path = 0;
-					destroyQuack(quackArray[*path]);
+					makeEmptyQuack(quackArray[*path]);
 					quackArray[*path] = createQuack();
 					for (int i = 0; i < numV; i++) {
 						qush(visited[i], quackArray[*path]);
@@ -183,18 +252,11 @@ void dfsR(Graph g, Vertex v, int numV, int counter, int *maxSeen, int *visited, 
 				} else if (counter+1 == *maxSeen) {
 					
 					*path = *path + 1;
-					destroyQuack(quackArray[*path]);
+					makeEmptyQuack(quackArray[*path]);
 					quackArray[*path] = createQuack();
 					for (int i = 0; i < numV; i++) {
 						qush(visited[i], quackArray[*path]);
 					}
-					
-					
-					//printf("\nmax node found %d | ", counter+1);
-					for (int i = 0; i < numV; i++) {
-    					//printf("%d->", visited[i]);
-    				}
-    				//printf(" --| cursor = %d ", *cursor);
 				}
        		}
        	} 
@@ -202,23 +264,29 @@ void dfsR(Graph g, Vertex v, int numV, int counter, int *maxSeen, int *visited, 
     return;
 }
 
-
+/*
+Input: Graph, vertix to start with (integer), number of vertixes (integer), array of quacks to store the paths (array of quacks), number of paths (pointer to the integer), previous 			maximum seen value of length of path (pointer to the integer)   
+Return Value: maximum seen value of length of path
+Usage: to call the recursive function and get the values set/destroy before/after calling it
+*/
 int dfs(Graph g, Vertex rootv, int numV, Quack *quackArray, int *path, int *maxSeen) {
-    //int maxSeen = 1;
     int counter = 0;
-    int *visited = mallocArray(numV); //TODO FREE THIS 
+    int *visited = mallocArray(numV); 
     int cursor = 0;
-    Vertex startv = rootv; 
-    //int path = 0;                     
+    Vertex startv = rootv;                    
     dfsR(g, startv, numV, counter+1, maxSeen, visited, &cursor, quackArray, path);
     
     //printf("maxSeen = %d, cursor = %d, path = %d \n", *maxSeen, cursor, *path);
-    //showQuack(quackArray[0]);
-    // TODO: support disconnected graphs
+    free(visited);
    return *maxSeen;
 }
 
 
+/*
+Input: Pointer to the quack array   
+Return Value: Initiated quacks in all the indexes of the array
+Usage: to give memory to all the quacks at once
+*/
 Quack *createQuackArray(Quack *quackArray) {
 	for (int i = 0; i < MAXIMUM_PATHS; i++) {
 		quackArray[i] = createQuack();
@@ -227,7 +295,12 @@ Quack *createQuackArray(Quack *quackArray) {
 }
 
 
-void printArray(int maxSeen, int n, char **dict, Quack *quackArray, int path) {
+/*
+Input: maximum seen value of length of path (integer), dictionary of words stored in array of character pointers, array of quacks, and number of paths (integer)
+Return Value: void
+Usage: print the longest paths in the graph with dictionary 
+*/
+void printArray(int maxSeen, char **dict, Quack *quackArray, int path) {
 	printf("Longest ladder length: %d\nLongest ladders:\n", maxSeen);
 	for (int i = 0; i <= path && i<99; i++) {
 		printf("%2d: ", i+1);
@@ -236,9 +309,7 @@ void printArray(int maxSeen, int n, char **dict, Quack *quackArray, int path) {
 			printf("%s -> ", dict[pop(quackArray[i])]);
 		}
 		printf("%s\n", dict[pop(quackArray[i])]);
-		//printf("\n");
 	}
-	//printf("\n");
 }
 
 
@@ -261,17 +332,14 @@ int main(void) {
     	}
     	if (maxSeen == -1) {
     		printf("Longest ladder length: 1\nLongest ladders:\n");
-    		for (int i = 0; i < wordCount; i++) {
+    		for (int i = 0; i < wordCount && i < 99; i++) {
     			printf("%2d: %s\n",i+1, dict[i]);
     		}
     		
     	} else {
-    		printArray( maxSeen, wordCount, dict, quackArray, path);
+    		printArray( maxSeen, dict, quackArray, path);
     	}
-		
     }
-    //printf("path %d \n", path);
-    //printf("maxseen %d \n", maxSeen);
     dict = freeDict(dict, wordCount);
     
 	return EXIT_SUCCESS;
